6f47511e980e98c2fdfc346f9ad33e698b5ea69a
auntfunny <greenauntfunny@gmail.com>
2026-02-04 18:58:44 -0300

    Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, resolveConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path';
 export default defineConfig({
   plugins: [tailwindcss()],
+  build:{
+    roolupOptions:{
+      input:{
+        main: resolve(__dirname,'index.html'),
+        projects: resolve(__dirname, 'pages/my_projects.html'),
+        links: resolve(__dirname, 'pages/social_links.html')
+      }
+    }
+  }
 });
 
